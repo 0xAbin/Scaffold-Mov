b87c512c94d492db63d742bdab4993bc2c1ec35e
0xabin <abinbvinod@gmail.com>
2024-05-11 10:38:54 +0530

    Movement Labs - Test

---
+++ packages/nextjs/scaffold.config.ts
@@ -0,0 +1,47 @@
+import * as chains from "viem/chains";
+import { type Chain } from "viem"
+
+export type ScaffoldConfig = {
+  targetNetworks: readonly chains.Chain[];
+  pollingInterval: number;
+  alchemyApiKey: string;
+  walletConnectProjectId: string;
+  onlyLocalBurnerWallet: boolean;
+  walletAutoConnect: boolean;
+};
+
+export const mevm = {
+  id: 336,
+  name: "M1 MEVM",
+  network: "mevm",
+  nativeCurrency: { name: "Move", symbol: "MOVE", decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ["https://mevm.devnet.m1.movementlabs.xyz"],
+    },
+    public: {
+      http: ["https://mevm.devnet.m1.movementlabs.xyz"],
+    },
+  },
+  blockExplorers: {
+    etherscan: { name: "Arbiscan", url: "https://explorer.mevm.devnet.m1.movementlabs.xyz" },
+    default: { name: "Arbiscan", url: "https://explorer.mevm.devnet.m1.movementlabs.xyz" },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xD67Eca381AAbd0c049554f5714E7d8C21864c560",
+      blockCreated: 500,
+    },
+  },
+} as const satisfies Chain
+
+
+const scaffoldConfig = {
+  targetNetworks: [mevm],
+  pollingInterval: 30000,
+  alchemyApiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF",
+  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "3a8170812b534d0ff9d794f19a901d64",
+  onlyLocalBurnerWallet: true,
+  walletAutoConnect: true,
+} as const satisfies ScaffoldConfig;
+export default scaffoldConfig;
